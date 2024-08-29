b01f815b30a688e086b0d40328a24abb1312253a
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-29 07:04:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,46 @@
+let score =null;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// 33 => 33
+//"33ds" => NaN
+//Null => 0
+//true => 1 false => 0
+//undefined => NaN
+
+let isLoggedIn = 1
+let loggedIn = Boolean(isLoggedIn)
+console.log(loggedIn);
+console.log(typeof loggedIn);
+
+let isStudying = 0
+let output = Boolean(isStudying)
+console.log(output);
+console.log(typeof output);
+
+let emptyString = "akhil kumar"
+let Output = Boolean(emptyString)
+console.log(Output)
+console.log(typeof Output)
+
+// 1 = true ; 0 = false
+// "" = false ; "alhilfd" = true
+
+let someNumber = 4343;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
