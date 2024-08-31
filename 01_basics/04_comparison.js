8a6317ae41a4bca80e5da825c18cbf04462d5641
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-31 09:33:37 +0000

    datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2 == 2);
+// console.log(2 !=3);
+// console.log(2 <= 3);
+// console.log(2 >= 4 );//all these logs will have the boolean value whether it is true or false
+
+// console.log("2" === 2);/// here the datatype is also checked 
+// console.log("2" == 2);/// here the string is converted is to the number
+
+
+
+// console.log(null > 0);//// here the comparison operator convert the null into the 0
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+ 
+
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// #Primitive:
+// 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+// const score = 322
+// const scorevalue = 232.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let emailadd;
+
+const id = Symbol('232')
+const anotherId = Symbol('232')
+
+// console.log(id === anotherId);
+
+const bigNumber = 23233232345745n
+
+
+//javascript is dynamically typed language this means that we donot have to define the datatype of the variable
+
+// reference types(non- primitive):
+//Array,objects,functions
+
+const heroes = ["shaktiman", "krish","naagraaj"]///Array
+
+let myObj = {
+    name: "akhil kumar",
+    age: 23,
+}///objects
+
+///functions
+
+const myFunction =  function(){
+
+console.log("akhil kumar");
+}
+console.log(typeof myFunction);
+console.log(typeof heroes);
+console.log(typeof id);
+
