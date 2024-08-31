ca9123b4f0ccf8784755924d10053a2ab70bccce
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-31 17:12:23 +0000

    nums and maths operations

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,57 @@
+const score = 400
+console.log(score);
+
+
+const value = new Number(200)
+// console.log(value);
+
+// console.log(value.toString().length);
+// console.log(value.toFixed(3));
+// console.log(typeof value);
+
+
+
+const OtherNumber = 21223.93232
+// console.log(OtherNumber.toPrecision(5));
+
+const price = 1000000
+// console.log(price.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+/**************************MATHS**************************** */
+
+console.log(Math);
+console.log(Math.abs(-3232));//// here the -ve value is converted into the +ve and the +ve remains the +ve
+
+console.log(Math.round(54.922));//// roundofff
+console.log(Math.ceil(54.3));/// if the number is more than the given number than it will increase the number
+console.log(Math.floor(32.99));/// this will take the base value
+console.log(Math.pow(3,3));
+
+console.log(Math.min(9,2,4,5,3));///gives the lowest value of the array
+console.log(Math.max(9,2,4,5,3));/// gives the largest value of the array
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+console.log(Math.ceil((Math.random()*10)) + 1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
