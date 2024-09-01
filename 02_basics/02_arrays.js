2619b16cb0e5a7ac1a1b38a1e7dc9411e5a4ca61
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-09-01 08:13:27 +0000

    Arrays-2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,21 +1,21 @@
  const marvel_hereos = ["thor","ironman","spiderman"]
  const dc_hereos = ["superman","batman","wonder-women"]
 
-//  marvel_hereos.push(dc_hereos)/// here the arrays is treated as the data in the array i.e the new array will be the new element
-//  console.log(marvel_hereos);
-//  console.log(marvel_hereos[3][2]);
+ marvel_hereos.push(dc_hereos)/// here the arrays is treated as the data in the array i.e the new array will be the new element
+ console.log(marvel_hereos);
+ console.log(marvel_hereos[3][2]);
 
-// const all_heroes = marvel_hereos.concat(dc_hereos)/// concat method returns the new array thats why we are making the new array
-// console.log(all_heroes);
+const all_heroes = marvel_hereos.concat(dc_hereos)/// concat method returns the new array thats why we are making the new array
+console.log(all_heroes);
 
-///SPREAD OPERATOR
+//SPREAD OPERATOR
 
-// const all_new_heroes = [...marvel_hereos,...dc_hereos]/// this is the short-hand property to merge two arrays
-// console.log(all_new_heroes);
+const all_new_heroes = [...marvel_hereos,...dc_hereos]/// this is the short-hand property to merge two arrays
+console.log(all_new_heroes);
 
 const anotherArr = [1,2,3,[12,343],1,22,[222,1,[232,222,222,323,[23232,3232,3232]]]]
 const real_arr = anotherArr.flat(Infinity)/// this converts the all the internal arrays into the single array upto any depth
-// console.log(real_arr);
+console.log(real_arr);
 
 
 console.log(Array.isArray("akhil kumar"));// rerurs the boolean value
