9f8e9ad1afe91c69fa9b14626ece2f5d34c10e8d
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-09-01 12:25:17 +0000

    objects-1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,50 @@
+// object literals
+// Object.create/// this is through the constructor method
+
+const mySym = Symbol("mykey-1") 
+const jsUser = { /// here the keys are treated as the strings
+    name : "akhil kumar",
+    "full-name" : "akhil kumar dhiman",
+    age : 23,
+    email : "akhil@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday","saturday"],
+    location: "noida",
+    [mySym] : "key-1"//this is the syntax how we can use the symbol in the objects, first we have to declare the symbol first before the object creation
+}
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full-name"]);
+// console.log(jsUser[mySym]);
+
+
+/// changes in the objects 
+jsUser.email = "akhil@amazon.com"
+// console.log(jsUser["email"]);
+
+// freezing the object
+
+// Object.freeze(jsUser)//  now the object is freezed and we cannot make the changes after this
+jsUser.email = "akhil@flipkart.com"
+// console.log(jsUser["email"]);
+// console.log(jsUser);
+
+
+jsUser.greetings = function(){
+    console.log("hello JS user");
+}
+console.log(jsUser.greetings());
+
+jsUser.greetingsTwo = function(){
+    console.log(`hello jsUser ${this["full-name"]}`);
+}
+console.log(jsUser.greetingsTwo());
+
+
+
+
+
+
+
+
+
