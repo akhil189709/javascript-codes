dabf9ec065df5de39267915dffc49905bbef1f98
akhil189709 <akhildhiman1897@gmail.com>
2024-09-10 18:15:10 +0530

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,37 @@
+///for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+    
+// }
+// for (let i = 0; i <= 20; i++) {
+// console.log(`outer loop value: ${i}`);
+// for (let j = 0; j <=10; j++) {
+    
+//     console.log(i + " * " + j + " = " +i*j);
+//     }
+    
+// }
+
+// let myArray = ["flash","superman","batman"]
+// console.log(myArray.length);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+    
+    
+// }
+
+//break and continue
+
+for (let i = 0; i<=20; i++) {
+    if(i == 12){
+        break;
+    }
+    console.log(i);
+    
+
+}
